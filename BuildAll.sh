8bced56beffe291cbf75c593770bb9c2c04adf9a
offtkp <parisoplop@gmail.com>
2025-05-19 01:15:20 +0300

    Hello world

---
+++ BuildAll.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Give them a unique id for clarity
+ID="ubuntu-24.04-$(date +%s%N)"
+
+set -e
+
+sudo docker build -t $ID ./Ubuntu-24.04
+sudo docker create --name Ubuntu-24.04 $ID
+sudo docker export Ubuntu-24.04 | gzip > Ubuntu-24.04.tar.gz
+sudo docker rm Ubuntu-24.04
---
+++ README.md
@@ -0,0 +1,3 @@
+# felix86 rootfs image generation
+
+Run the BuildAll.sh script to build all the rootfs images
