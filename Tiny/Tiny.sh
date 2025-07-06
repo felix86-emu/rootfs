f664fa2daeb428b4190ddbb5ea5365158eb39586
offtkp <parisoplop@gmail.com>
2025-07-06 23:36:35 +0300

    Add wine

--- Tiny/Tiny.sh
+++ Tiny/Tiny.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+apt-get update
+apt-get install --no-install-recommends -y wine64 
+
 # Create a .tar.gz inside the container while excluding the tempfs directories
 touch archive.tar.gz
 tar  --exclude=archive.tar.gz --exclude=./media --exclude=./mnt --exclude=./root --exclude=./srv --exclude=./boot --exclude=./home --exclude=./run --exclude=./proc --exclude=./sys --exclude=./dev --exclude=./tmp --exclude=./.dockerenv -czf archive.tar.gz .
