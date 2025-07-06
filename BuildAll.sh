1ff347202262c4a2455248ebf58eeec1492da47d
offtkp <parisoplop@gmail.com>
2025-07-06 18:34:09 +0300

    Add tiny rootfs

--- BuildAll.sh
+++ BuildAll.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 # Give them a unique id for clarity
-ID="ubuntu-24.04-$(date +%s%N)"
+UBUNTU_ID="felix86-ubuntu-24.04-$(date +%s%N)"
+TINY_ID="felix86-tiny-$(date +%s%N)"
 
 set -e
 
-sudo docker build -t $ID ./Ubuntu-24.04
-sudo docker create --name Ubuntu-24.04 $ID
+sudo docker build -t $UBUNTU_ID ./Ubuntu-24.04
+sudo docker create --name Ubuntu-24.04 $UBUNTU_ID
 sudo docker cp Ubuntu-24.04:/archive.tar.gz ./Ubuntu-24.04.tar.gz
 sudo docker rm Ubuntu-24.04
+
+sudo docker build -t $UBUNTU_ID ./Tiny
+sudo docker create --name Tiny $UBUNTU_ID
+sudo docker cp Tiny:/archive.tar.gz ./Tiny.tar.gz
+sudo docker rm Tiny
---
+++ Tiny/Tiny.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Create a .tar.gz inside the container while excluding the tempfs directories
+touch archive.tar.gz
+tar  --exclude=archive.tar.gz --exclude=./media --exclude=./mnt --exclude=./root --exclude=./srv --exclude=./boot --exclude=./home --exclude=./run --exclude=./proc --exclude=./sys --exclude=./dev --exclude=./tmp --exclude=./.dockerenv -czf archive.tar.gz .
