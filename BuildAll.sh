6eb427478895f7d592fe51d668d1314469c63a07
offtkp <parisoplop@gmail.com>
2025-11-24 02:38:51 +0200

    Update

--- BuildAll.sh
+++ BuildAll.sh
@@ -10,6 +10,11 @@ sudo docker create --name Ubuntu-24.04 $UBUNTU_ID
 sudo docker cp Ubuntu-24.04:/archive.tar.gz ./Ubuntu-24.04.tar.gz
 sudo docker rm Ubuntu-24.04
 
+sudo docker build -t $UBUNTU_ID ./Ubuntu-24.04-NoWine
+sudo docker create --name Ubuntu-24.04-NoWine $UBUNTU_ID
+sudo docker cp Ubuntu-24.04-NoWine:/archive.tar.gz ./Ubuntu-24.04-NoWine.tar.gz
+sudo docker rm Ubuntu-24.04-NoWine
+
 sudo docker build -t $UBUNTU_ID ./Tiny
 sudo docker create --name Tiny $UBUNTU_ID
 sudo docker cp Tiny:/archive.tar.gz ./Tiny.tar.gz
