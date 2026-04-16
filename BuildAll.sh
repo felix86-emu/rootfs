fc843c11869dc56be8aac6cef1b5b35e0a0a5705
Paris Oplopoios <21157395+OFFTKP@users.noreply.github.com>
2026-04-17 01:09:00 +0300

    Fix broken package

--- BuildAll.sh
+++ BuildAll.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
-# Give them a unique id for clarity
-UBUNTU_ID="felix86-ubuntu-24.04-$(date +%s%N)"
-
 set -e
 
-sudo docker build -t $UBUNTU_ID ./Ubuntu-24.04
-sudo docker create --name Ubuntu-24.04 $UBUNTU_ID
-sudo docker cp Ubuntu-24.04:/archive.tar.gz ./Ubuntu-24.04.tar.gz
-sudo docker rm Ubuntu-24.04
+build_and_export() {
+    local folder=$1
+    local name="${folder,,}"
+    local out="${folder}.tar.gz"
+    sudo docker build -t "$name" "./$folder"
+    sudo docker create --name "$name" "$name"
+    sudo docker export "$name" | bsdtar -czf - \
+        --exclude=media --exclude=mnt --exclude=root --exclude=srv \
+        --exclude=boot --exclude=home --exclude=run --exclude=proc \
+        --exclude=sys --exclude=dev --exclude=tmp --exclude=.dockerenv \
+        @- > "$out"
+    sudo docker rm "$name"
+    sudo docker rmi "$name"
+}
 
-sudo docker build -t $UBUNTU_ID ./Ubuntu-24.04-NoWine
-sudo docker create --name Ubuntu-24.04-NoWine $UBUNTU_ID
-sudo docker cp Ubuntu-24.04-NoWine:/archive.tar.gz ./Ubuntu-24.04-NoWine.tar.gz
-sudo docker rm Ubuntu-24.04-NoWine
-
-sudo docker build -t $UBUNTU_ID ./Tiny
-sudo docker create --name Tiny $UBUNTU_ID
-sudo docker cp Tiny:/archive.tar.gz ./Tiny.tar.gz
-sudo docker rm Tiny
-
-FEDORA_ID="felix86-fedora-43-$(date +%s%N)"
-
-sudo docker build -t $FEDORA_ID ./Fedora-Tiny
-sudo docker create --name FedoraTiny $FEDORA_ID
-sudo docker cp FedoraTiny:/archive.tar.gz ./FedoraTiny.tar.gz
-sudo docker rm FedoraTiny
+build_and_export Ubuntu-24.04
+build_and_export Ubuntu-24.04-NoWine
+build_and_export Tiny
--- Tiny/Tiny.sh
+++ Tiny/Tiny.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
+set -e
+
 apt-get update
 apt-get upgrade -y
-
-# Create a .tar.gz inside the container while excluding the tempfs directories
-touch archive.tar.gz
-tar  --exclude=archive.tar.gz --exclude=./media --exclude=./mnt --exclude=./root --exclude=./srv --exclude=./boot --exclude=./home --exclude=./run --exclude=./proc --exclude=./sys --exclude=./dev --exclude=./tmp --exclude=./.dockerenv -czf archive.tar.gz .
